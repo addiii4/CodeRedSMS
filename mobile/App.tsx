b172bee15776b7584bec5a8a832da84c01e62a69
addiii4 <adityacheema4@gmail.com>
2025-08-06 13:16:21 +1000

    Chore: Performed clean up of node modules

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import AppNavigator from './src/navigation/AppNavigator';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +10,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return <AppNavigator />;
+}
