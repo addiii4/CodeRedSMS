997ec4fb1fadaca789a41fb9eb81a61344d220bc
addiii4 <adityacheema4@gmail.com>
2025-08-07 00:50:09 +1000

    Feat: Added constants for styling

---
+++ mobile/src/constants/typography.ts
@@ -0,0 +1,40 @@
+const Typography = {
+    display: {
+        fontFamily: 'SFProDisplay-Bold',
+        fontSize: 34,
+        fontWeight: '700',
+        color: '#121212'
+    },
+    title: {
+        fontFamily: 'SFProDisplay-Semibold',
+        fontSize: 28,
+        fontWeight: '600',
+        color: '#121212'
+    },
+    sectionTitle: {
+        fontFamily: 'SFProDisplay-Regular',
+        fontSize: 22,
+        fontWeight: '400',
+        color: '#121212'
+    },
+    body: {
+        fontFamily: 'SFProDisplay-Regular',
+        fontSize: 18,
+        fontWeight: '400',
+        color: '#121212'
+    },
+    captions: {
+        fontFamily: 'SFProDisplay-Regular',
+        fontSize: 14,
+        fontWeight: '400',
+        color: '#D6D6D6'
+    },
+    label: {
+        fontFamily: 'SFProDisplay-Medium',
+        fontSize: 12,
+        fontWeight: '500',
+        color: '#666666'
+    }
+};
+
+export default Typography;
