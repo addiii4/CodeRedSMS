0449784be35f8306353c08af4eafbb0a9236f715
addiii4 <adityacheema4@gmail.com>
2025-08-06 13:19:37 +1000

    Chore: Fixed Imports

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
