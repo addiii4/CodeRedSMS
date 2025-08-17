f86cbac5a70f95ca9eecef8a02ba12f88eaea84b
addiii4 <adityacheema4@gmail.com>
2025-08-17 23:38:42 +1000

    Feat: Added main

---
+++ server/src/main.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule, { cors: true });
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    await app.listen(3000);
+    console.log('API on http://localhost:3000/api');
+}
+bootstrap();
