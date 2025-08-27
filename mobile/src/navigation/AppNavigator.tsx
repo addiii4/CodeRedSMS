4742516a57db21521df88d16299048ef7bce34d2
addiii4 <adityacheema4@gmail.com>
2025-08-27 22:13:23 +1000

    Chore: Fixed file structure

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -27,39 +27,42 @@ import PaymentMethods from '../screens/PaymentMethods';
 import Profile from '../screens/Profile';
 import PurchaseHistory from '../screens/PurchaseHistory';
 import LogDetail from '../screens/LogDetail';
+import { AuthProvider } from './src/state/auth.tsx';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
     return (
-        <NavigationContainer>
-        <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Splash" component={Splash} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Signup" component={SignupScreen} />
-            <Stack.Screen name="Dashboard" component={Dashboard} />
-            <Stack.Screen name="Compose" component={Compose} />
-            <Stack.Screen name="TemplatePreview" component={TemplatePreview} />
-            <Stack.Screen name="SelectGroups" component={SelectGroups} />
-            <Stack.Screen name="ScheduleReview" component={ScheduleReview} />
-            <Stack.Screen name="Settings" component={Settings} />
-            <Stack.Screen name="Logs" component={Logs} />
-            <Stack.Screen name="Templates" component={Templates} />
-            <Stack.Screen name="TemplateEdit" component={TemplateEdit} />
-            <Stack.Screen name="Contacts" component={Contacts} />
-            <Stack.Screen name="GroupDetail" component={GroupDetail} />
-            <Stack.Screen name="BuyCredits" component={BuyCredits} />
-            <Stack.Screen name="GroupEdit" component={GroupEdit} />
-            <Stack.Screen name="PersonEdit" component={PersonEdit} />
-            <Stack.Screen name="Profile" component={Profile} />
-            <Stack.Screen name="ChangePassword" component={ChangePassword} />
-            <Stack.Screen name="PaymentMethods" component={PaymentMethods} />
-            <Stack.Screen name="PurchaseHistory" component={PurchaseHistory} />
-            <Stack.Screen name="HelpCenter" component={HelpCenter} />
-            <Stack.Screen name="ContactUs" component={ContactUs} />
-            <Stack.Screen name="ContactImport" component={ContactImport} />
-            <Stack.Screen name="LogDetail" component={LogDetail} />
-        </Stack.Navigator>
-        </NavigationContainer>
+        <AuthProvider>  
+            <NavigationContainer>
+            <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Splash" component={Splash} />
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Signup" component={SignupScreen} />
+                <Stack.Screen name="Dashboard" component={Dashboard} />
+                <Stack.Screen name="Compose" component={Compose} />
+                <Stack.Screen name="TemplatePreview" component={TemplatePreview} />
+                <Stack.Screen name="SelectGroups" component={SelectGroups} />
+                <Stack.Screen name="ScheduleReview" component={ScheduleReview} />
+                <Stack.Screen name="Settings" component={Settings} />
+                <Stack.Screen name="Logs" component={Logs} />
+                <Stack.Screen name="Templates" component={Templates} />
+                <Stack.Screen name="TemplateEdit" component={TemplateEdit} />
+                <Stack.Screen name="Contacts" component={Contacts} />
+                <Stack.Screen name="GroupDetail" component={GroupDetail} />
+                <Stack.Screen name="BuyCredits" component={BuyCredits} />
+                <Stack.Screen name="GroupEdit" component={GroupEdit} />
+                <Stack.Screen name="PersonEdit" component={PersonEdit} />
+                <Stack.Screen name="Profile" component={Profile} />
+                <Stack.Screen name="ChangePassword" component={ChangePassword} />
+                <Stack.Screen name="PaymentMethods" component={PaymentMethods} />
+                <Stack.Screen name="PurchaseHistory" component={PurchaseHistory} />
+                <Stack.Screen name="HelpCenter" component={HelpCenter} />
+                <Stack.Screen name="ContactUs" component={ContactUs} />
+                <Stack.Screen name="ContactImport" component={ContactImport} />
+                <Stack.Screen name="LogDetail" component={LogDetail} />
+            </Stack.Navigator>
+            </NavigationContainer>
+        </AuthProvider>  
     );
 }
