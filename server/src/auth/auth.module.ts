3aa17f4450b6ef5bff477e48c0fbfa37e608f7a3
addiii4 <adityacheema4@gmail.com>
2025-09-08 19:44:34 +1000

    Hotfix: Dependency injection was not working, removed it to test

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,17 +5,20 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { OrgsModule } from '../orgs/orgs.module';
 import { JwtStrategy } from './jwt.strategy';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
     imports: [
+        PrismaModule, 
         UsersModule,
         OrgsModule,
         JwtModule.register({
             secret: process.env.JWT_SECRET || 'dev-secret',
-            signOptions: { expiresIn: process.env.JWT_EXPIRES || '15m' },
+            signOptions: { expiresIn: process.env.JWT_EXPIRES || '15m' }
         }),
     ],
     providers: [AuthService, JwtStrategy],
     controllers: [AuthController],
+    exports: [AuthService],
 })
 export class AuthModule {}
