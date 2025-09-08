f4fe0230ccc0b11371c3378ba1f640cab4d05b33
addiii4 <adityacheema4@gmail.com>
2025-09-08 19:45:35 +1000

    Bug fixes

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
+    app.enableCors({
+        origin: '*',
+        credentials: false
+    });
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
     await app.listen(3000);
--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,7 +1,18 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+    extends PrismaClient<Prisma.PrismaClientOptions, 'query' | 'info' | 'warn' | 'error'>
+    implements OnModuleInit
+    {
+    async onModuleInit() {
+        await this.$connect();
+    }
 
+    async enableShutdownHooks(app: INestApplication) {
+    (this.$on as any)('beforeExit', async () => {
+        await app.close();
+    });
+}
 }
