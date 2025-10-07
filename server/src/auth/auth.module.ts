c34aa13ae768a8c0b4ea465d75b92fdbd2d70455
addiii4 <adityacheema4@gmail.com>
2025-10-07 22:25:19 +1000

    fix(auth): correct AuthController to use DTO classes and proper injected AuthService

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -6,18 +6,24 @@ import { UsersModule } from '../users/users.module';
 import { OrgsModule } from '../orgs/orgs.module';
 import { JwtStrategy } from './jwt.strategy';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from '../prisma/prisma.service';
 
+@Global()
 @Module({
     imports: [
         PrismaModule, 
         UsersModule,
         OrgsModule,
+        PassportModule,
+        ConfigModule,
         JwtModule.register({
             secret: process.env.JWT_SECRET || 'dev-secret',
             signOptions: { expiresIn: process.env.JWT_EXPIRES || '15m' }
         }),
     ],
-    providers: [AuthService, JwtStrategy],
+    providers: [AuthService, JwtStrategy, PrismaService],
     controllers: [AuthController],
     exports: [AuthService],
 })
